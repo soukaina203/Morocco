e0bc814f595e08fe160d59c0f321fa817da395ee
soukaina203 <soukaina9mourabit@gmail.com>
2022-12-23 14:59:44 +0000

    hh

--- src/RoutingPages/header/Header.js
+++ src/RoutingPages/header/Header.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import './header.css'
+import {Link} from 'react-router-dom';
 export default class Header extends Component {
   render() {
     return (
       <div className='Header'>
         <img src="0.png" alt="" />
         <ul>
-          <li> <a href="/" >Home</a>  </li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          {/* <li> <a href="/" >Home</a>  </li>
           <li> <a href="/about">About</a>  </li>
-          <li> <a href="/contact">Contact</a>  </li>
+          <li> <a href="/contact">Contact</a>  </li> */}
         </ul>
       </div>
     )
